4f32b70f99c4550f839c232b9250dccaa55350b0
HirooshaMadhushan <HirooshaMadhushan@users.noreply.github.com>
2026-06-03 10:00:00 +0530

    feat: setup production environment and terraform backend

---
+++ infra/environments/prod/variables.tf
@@ -0,0 +1,40 @@
+variable "common_tags" {
+  type = map(string)
+  default = {
+    Project     = "cloudmart"
+    Environment = "prod"
+    Team        = "cloudmart-team"
+    ManagedBy   = "terraform"
+  }
+}
+
+variable "environment" {
+  type    = string
+  default = "prod"
+}
+
+variable "admin_cidr" {
+  type        = string
+  description = "CIDR block for admin access"
+}
+
+variable "db_password" {
+  type        = string
+  sensitive   = true
+  description = "Password for RDS database"
+}
+
+variable "ses_domain" {
+  type        = string
+  description = "Domain for SES"
+}
+
+variable "ses_test_email" {
+  type        = string
+  description = "Test email for SES"
+}
+
+variable "alert_email" {
+  type        = string
+  description = "Email for alerts"
+}
