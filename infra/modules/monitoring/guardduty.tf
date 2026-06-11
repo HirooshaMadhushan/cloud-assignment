aa1d5d3245f0bcd052fe2504fa89ec8eeeddba9e
RandilG <RandilG@users.noreply.github.com>
2026-06-11 09:23:01 +0530

    feat: add cloudwatch and guardduty monitoring

---
+++ infra/modules/monitoring/guardduty.tf
@@ -0,0 +1,12 @@
+resource "aws_guardduty_detector" "main" {
+  enable = true
+  tags   = var.common_tags
+
+  datasources {
+    s3_logs           { enable = true }
+    kubernetes { audit_logs { enable = true } }
+    malware_protection {
+      scan_ec2_instance_with_findings { ebs_volumes { enable = true } }
+    }
+  }
+}
